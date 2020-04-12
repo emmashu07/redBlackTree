d4f5b08af8718dca00341c40b4b7f101ff26e4c4
emmashu07 <emmashu@DESKTOP-TTUPTH2.localdomain>
2020-04-12 15:13:02 -0700

    Pulled functions from binary search tree

--- main.cpp
+++ main.cpp
@@ -7,55 +7,94 @@ using namespace std;
 
 struct Node {
 	int data;
+	int redBlack;
 	Node* left;
 	Node* right;
-	int redBlack;
 	Node* parent;
 };
 
 int convertToInt(char* num);
 int inputToInt(char* input, int* array);
 int fileToInt(char* fileName, int* array);
-void add(Node* &currRoot, int num);
-bool search(Node* currRoot, int num);
-void print(Node* currRoot, int tabNum);
-void build(int* array, int size, Node* &root);
-bool remove(Node* &currRoot, int num); 
-void removeNode(Node* &currRoot);
+void read(Node* root, int* array, int size);
 void display(Node* currRoot);
-Node*& largest(Node* root);
+void print(Node* currRoot, int tabNum);
+Node* getParent(Node* node);
+Node* getGrandparent(Node* node);
+Node* getSibling(Node* node);
+Node* getUncle(Node* node);
+void rotateLeft(Node* node);
+void rotateRight(Node* node);
+
+const int BLACK = 0;
+const int RED = 1;
 
 int main() {
-	char input[200];
 	char fileName[50];
-	char forC[10];
-	char input2[20];
+	char input[20];
 	int* array = new int[100];
 	int size;
 	Node* root = NULL;
 	bool running = true;
+	
+	cout << "Please enter a file name: ";
+	cin.get(fileName, 50);
+	cin.ignore(50, '\n');
+	size = fileToInt(fileName, array);	
 
-	cout << "Please enter whether you would like to input through file or console: ";
-	cin.get(forC, 10);
-	cin.ignore(10, '\n');
-
-	if (strcmp(forC, "FILE") == 0) {
-		cout << "Please enter a file name: ";
-		cin.get(fileName, 50);
-		cin.ignore(50, '\n');
-		size = fileToInt(fileName, array);
-	}
-	else if (strcmp(forC, "CONSOLE") == 0) {
-		cout << "Please enter a series of numbers separated by a space: ";
-		cin.get(input, 200);
-		cin.ignore(200, '\n');
-		size = inputToInt(input, array);	
-	}
-	else {
-		cout << "Please enter FILE or CONSOLE." << endl;
-	}	
+	read(root, array, size);
 
 	while (running) {
+		cout << "Please enter whether you would like to search, remove, insert, print, or quit: ";
+		cin.get(input, 20);
+		cin.ignore(20, '\n');
+		
+		if (strcmp(input, "INSERT") == 0) {
+			int num;
+			cout << "Please enter a number: ";
+			cin >> num;
+			cin.ignore(20, '\n');
+			//add(root, num);
+		}
+		else if (strcmp(input, "SEARCH") == 0) {
+			/*bool in;
+			int num;
+			cout << "Please enter a number: ";
+			cin >> num;
+			cout << endl;
+			cin.ignore(20, '\n');
+			in = search(root, num);
+			if (in) {
+				cout << "The number is in the tree." << endl;
+			}
+			else {
+				cout << "The number is not in the tree." << endl;
+			}*/	
+		}
+		else if (strcmp(input, "REMOVE") == 0) {
+			/*bool in;
+			int num;
+			cout << "Please enter a number: ";
+			cin >> num;
+			cout << endl;
+			cin.ignore(20, '\n');
+			in = remove(root, num);
+			if (in) {
+				cout << "Deleted." << endl;
+			}
+			else {
+				cout << "The number is not in the tree." << endl;
+			}*/
+		}	
+		else if (strcmp(input, "PRINT") == 0) {
+			print(root, 0);
+			cout << "Inorder traversal: ";
+			display(root);
+			cout << endl;
+		}
+		else if (strcmp(input, "QUIT") == 0) {
+			running = false;
+		}	
 	}
 	
 
@@ -109,4 +148,34 @@ int fileToInt(char* fileName, int* array) { // File input to int array, utilizes
 	}
 }	
 
+void print(Node* currRoot, int tabNum) { // Print the tree.
+	if (currRoot -> right) {
+		print(currRoot -> right, tabNum+1);
+	}
+	int i = 0;
+	while (i < tabNum) {
+		cout << '\t';
+		i++;
+	}
+	cout << currRoot -> data << endl;
+	if (currRoot -> left) {
+		print(currRoot -> left, tabNum+1);
+	}
+}
 
+void display(Node* currRoot) { // An inorder traversal of the tree.
+	if (currRoot) {
+		display(currRoot -> left);
+		cout << currRoot -> data << ' ';
+		display(currRoot -> right);
+	}
+}
+
+
+void read(Node* root, int* array, int size) {
+
+}
+
+
+
+		
